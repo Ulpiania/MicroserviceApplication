7e5115734ca0ae92106fca35ab216e78f4effbb4
Alex Ulpiani <alexulp@hotmail.com>
2022-06-23 12:49:08 +0100

    updated dependencies for git/sonarcloud

--- src/test/java/com/tsi/alex/program/SeliniumTest.java
+++ src/test/java/com/tsi/alex/program/SeliniumTest.java
@@ -1,18 +1,18 @@
-//package com.tsi.alex.program;
-//
-//import org.junit.jupiter.api.Test;
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//
-//public class SeliniumTest {
-//    WebDriver driver = new ChromeDriver();
-//
-//    @Test
-//    public void test() {
-//        driver.navigate().to("http://localhost:3000/services");
-//
-//        driver.findElement(By.id("btnActor")).click();
-//    }
-//}
+package com.tsi.alex.program;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeliniumTest {
+    WebDriver driver = new ChromeDriver();
+
+    @Test
+    public void test() {
+        driver.navigate().to("http://localhost:3000/services");
+
+        driver.findElement(By.id("btnActor")).click();
+ git a   }
+}
